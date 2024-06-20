3cd3a0d67a8cb8e65c17596422c5776e00b137c7
Renan Maganha <maganha.renan@gmail.com>
2024-06-20 09:36:41 -0300

    [PROJECT] add new project

--- 006-sprite-from-bin/ca65.sh
+++ 006-sprite-from-bin/ca65.sh
@@ -2,7 +2,7 @@
 #                             * ROM COMPILER *                             #
 # ==========================================================================
 # This script is a simple build automation tool for compiling and linking
-# 65816 assembly code using the cc65 toolchain. It takes the name of an
+# 65816 assembly code using the ca65 toolchain. It takes the name of an
 # assembly file as input, compiles it into an object file, links the object
 # file into a binary file, and then cleans up the temporary files it created.
 # ==========================================================================
@@ -11,8 +11,8 @@
 # To use this script, you would call it from the command line, passing the
 # name of your assembly file (without the .asm extension) as an argument.
 # For example, if your assembly file is named program.asm, you would
-# run the script like this: `./cc65.sh program`
-# If the permission denied error happens, just use: `chmod +x cc65.sh`
+# run the script like this: `./ca65.sh program`
+# If the permission denied error happens, just use: `chmod +x ca65.sh`
 # ==========================================================================
 
 
--- animating-sprite/main.s
+++ animating-sprite/main.s
@@ -5,11 +5,11 @@
 ; these are aliases for the used Memory Mapped Registers
 INIDSP          = $2100         ; Initial settings for the screen
 OBJSEL          = $2101         ; Object Size $ object data area designation
-OAMADDL         = $2102         ; Adress for acessing OAM
+OAMADDL         = $2102         ; Address for accessing OAM
 OAMADDH         = $2103
 OAMDATA         = $2104         ; Data for OAM write
 VMAINC          = $2115         ; VRAM address increment value designation
-VMADDL          = $2116         ; Adress for VRAM read and write
+VMADDL          = $2116         ; Address for VRAM read and write
 VMADDH          = $2117
 VMDATAL         = $2118         ; Data for VRAM write
 VMDATAH         = $2119         ; Data for VRAM write
@@ -23,6 +23,8 @@ RDNMI           = $4210         ; Read the NMI flag status
 ; Assembler Directives
 ; ==========================================================================
 .p816                           ; Set the assembler to 65816 code
+.A8
+.I16
 
 ; ==========================================================================
 ; Includes
@@ -38,16 +40,17 @@ ColorData:      .incbin "sprite.pal"
         sei
         clc
         xce
-        lda     #$8f
+        lda     #$008f
         sta     INIDSP
         stz     NMITIMEN
 
-        ; Transfer VRAM Data
+PrepareToHandleVRAM:
         stz     VMADDL
         stz     VMADDH
-        lda     #$80
+        lda     #$00C0           ; Increment VRAM address after each write
         sta     VMAINC
-        ldx     #$00
+        ldx     #$0000
+
 VRAMLoop:
         lda     SpriteData, X   ; Get bitplane 0/2 byte from the sprite data
         sta     VMDATAL         ; Write the byte in A to VRAM
@@ -55,13 +58,14 @@ VRAMLoop:
         lda     SpriteData, X   ; Get bitplane 1/3 byte from the sprite data
         sta     VMDATAH         ; Write the byte in A to VRAM
         inx                     ; Increment counter/offset
-        cpx     #$C0            ; Check whether we have written $18 * $20 = $120 bytes to VRAM (four sprites)
-        bcc     VRAMLoop        ; If X is smaller than $80, continue the loop
+        cpx     #$0300          ; Check whether we have written $18 * $20 = $120 bytes to VRAM (four sprites)
+        bcc     VRAMLoop        ; If X is smaller than $C0, continue the loop
 
-        ; Transfer CGRAM data
-        lda     #$80
+PrepareToHandleCGRAM:
+        lda     #$0080
         sta     CGADD
-        ldx     #$00
+        ldx     #$0000
+        
 CGRAMLoop:
         lda     ColorData, X    ; Get the color low byte
         sta     CGDATA          ; Store it in CGRAM
@@ -69,85 +73,153 @@ CGRAMLoop:
         lda     ColorData, X    ; Get the color high byte
         sta     CGDATA
         inx
-        cpx     #$20            ; Check whether 32/$20 bytes were transfered
+        cpx     #$0020          ; Check whether 32/$20 bytes were transferred
         bcc     CGRAMLoop
 
-        .byte $42, $00
+        .byte $0042, $0000
 
-        ; Setup OAM Data
+SetupOAMData:
         stz     OAMADDL
         stz     OAMADDH
 
+        ; ===============================================================
+        ; 1ST OBJ
+        ; ===============================================================
+
         ; OAM Data for first sprite
-        lda     # (256/2 - 8)   ; Horizontal position of first sprite
+        lda     #(256/2 - 8)    ; Horizontal position of first sprite
         sta     OAMDATA
-        lda     # (224/2 - 8)   ; Vertical position of first sprite
+        lda     #(224/2 - 8)    ; Vertical position of first sprite
         sta     OAMDATA
-        lda     #$00            ; Name of first sprite
+        lda     #$0000          ; Name of first sprite
         sta     OAMDATA
         lda     #$00            ; no flip, prio 0, palette 0
         sta     OAMDATA
 
         ; OAM data for second sprite
-        lda     # (256/2)       ; horizontal position of second sprite
+        lda     #(256/2)        ; horizontal position of second sprite
         sta     OAMDATA
-        lda     # (224/2 - 8)   ; vertical position of second sprite
+        lda     #(224/2 - 8)    ; vertical position of second sprite
         sta     OAMDATA
-        lda     #$01            ; name of second sprite
+        lda     #$0001          ; name of second sprite
         sta     OAMDATA
         lda     #$00            ; no flip, prio 0, palette 0
         sta     OAMDATA
 
         ; OAM data for third sprite
-        lda     # (256/2 - 8)   ; horizontal position of third sprite
+        lda     #(256/2 - 8)    ; horizontal position of third sprite
         sta     OAMDATA
-        lda     # (224/2)       ; vertical position of third sprite
+        lda     #(224/2)        ; vertical position of third sprite
         sta     OAMDATA
-        lda     #$02            ; name of third sprite
+        lda     #$0002          ; name of third sprite
         sta     OAMDATA
         lda     #$00            ; no flip, prio 0, palette 0
         sta     OAMDATA
 
         ; OAM data for fourth sprite
-        lda     # (256/2)       ; horizontal position of fourth sprite
+        lda     #(256/2)        ; horizontal position of fourth sprite
         sta     OAMDATA
-        lda     # (224/2)       ; vertical position of fourth sprite
+        lda     #(224/2)        ; vertical position of fourth sprite
         sta     OAMDATA
-        lda     #$03            ; name of fourth sprite
+        lda     #$0003          ; name of fourth sprite
         sta     OAMDATA
         lda     #$00            ; no flip, prio 0, palette 0
         sta     OAMDATA
 
         ; OAM data for fifth sprite
-        lda     # (256/2 - 8)
+        lda     #(256/2 - 8)
         sta     OAMDATA
-        lda     # (224/2 + 8)
+        lda     #(224/2 + 8)
         sta     OAMDATA
-        lda     #$04
+        lda     #$0004
         sta     OAMDATA
         lda     #$00
         sta     OAMDATA
 
         ; OAM data for fifth sprite
-        lda     # (256/2)
+        lda     #(256/2)
         sta     OAMDATA
-        lda     # (224/2 + 8)
+        lda     #(224/2 + 8)
         sta     OAMDATA
-        lda     #$05
+        lda     #$0005
+        sta     OAMDATA
+        lda     #$00
+        sta     OAMDATA
+
+        ; ===============================================================
+        ; 2ND OBJ
+        ; ===============================================================
+        ; OAM Data for first sprite
+        lda     #(256/2 - 8 + 16)   ; Horizontal position of first sprite
+        sta     OAMDATA
+        lda     #(224/2 - 8)        ; Vertical position of first sprite
+        sta     OAMDATA
+        lda     #$0006              ; Name of first sprite
+        sta     OAMDATA
+        lda     #$00                ; no flip, prio 0, palette 0
+        sta     OAMDATA
+
+        ; OAM data for second sprite
+        lda     #(256/2 + 16)       ; horizontal position of second sprite
+        sta     OAMDATA
+        lda     #(224/2 - 8)        ; vertical position of second sprite
+        sta     OAMDATA
+        lda     #$0007              ; name of second sprite
+        sta     OAMDATA
+        lda     #$00                ; no flip, prio 0, palette 0
+        sta     OAMDATA
+
+        ; OAM data for third sprite
+        lda     #(256/2 - 8 + 16)   ; horizontal position of third sprite
+        sta     OAMDATA
+        lda     #(224/2)            ; vertical position of third sprite
+        sta     OAMDATA
+        lda     #$0008              ; name of third sprite
+        sta     OAMDATA
+        lda     #$00                ; no flip, prio 0, palette 0
+        sta     OAMDATA
+
+        ; OAM data for fourth sprite
+        lda     #(256/2 + 16)       ; horizontal position of fourth sprite
+        sta     OAMDATA
+        lda     #(224/2)            ; vertical position of fourth sprite
+        sta     OAMDATA
+        lda     #$0009              ; name of fourth sprite
+        sta     OAMDATA
+        lda     #$00                ; no flip, prio 0, palette 0
+        sta     OAMDATA
+
+        ; OAM data for fifth sprite
+        lda     #(256/2 - 8 + 16)
+        sta     OAMDATA
+        lda     #(224/2 + 8)
+        sta     OAMDATA
+        lda     #$000A
+        sta     OAMDATA
+        lda     #$00
+        sta     OAMDATA
+
+        ; OAM data for fifth sprite
+        lda     #(256/2 + 16)
+        sta     OAMDATA
+        lda     #(224/2 + 8)
+        sta     OAMDATA
+        lda     #$000B
         sta     OAMDATA
         lda     #$00
         sta     OAMDATA
 
         ; Make objects visible
-        lda     #$10
+        lda     #$0010
         sta     TM
 
         ; Release forced blanking, set screen to full brightness
-        lda     #$0f
+        lda     #$000F
         sta     INIDSP
 
+
         ; Enable NMI, turn on automatic joypad polling
-        lda     #$81
+        lda     #$0081
         sta     NMITIMEN
 
         jmp     GameLoop
